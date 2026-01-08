76b4ab225396d37573d112a6c603ebfcc7290cb2
Alex-rom1 <alex.rom495@gmail.com>
2026-01-08 20:04:47 +0300

    added item model

---
+++ lib/src/data/models/item_model.dart
@@ -0,0 +1,34 @@
+import 'package:querylib_test/querylib_test.dart';
+
+class ItemModel extends BaseModel {
+  final DateTime created;
+  final String description;
+  final String id;
+  final int price;
+  final String title;
+
+  ItemModel({
+    required this.created,
+    required this.description,
+    required this.id,
+    required this.price,
+    required this.title,
+  });
+
+  ItemModel.fromJSON(Map<String, dynamic> json) : this(
+    created: DateTime.parse(json['created']),
+    description: json['description'],
+    id: json['id'],
+    price: json['price'],
+    title: json['title'],
+  );
+
+  @override
+  Map<String, dynamic> toJSON() => {
+    'created': created.toIso8601String(),
+    'description': description,
+    'id': id,
+    'price': price,
+    'title': title,
+  };
+}
