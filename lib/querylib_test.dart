73367dabbd61cbfb581271494348fe3d2f0e19c6
Alex-rom1 <alex.rom495@gmail.com>
2026-01-08 20:47:15 +0300

    added client requests

--- lib/querylib_test.dart
+++ lib/querylib_test.dart
@@ -4,6 +4,7 @@ export 'src/data/models/item_model.dart';
 export 'src/data/models/user_model.dart';
 
 export 'src/data/repository/repository.dart';
+export 'src/data/repository/client.dart';
 
 export 'src/data/storage/storage.dart';
 
---
+++ lib/src/data/repository/client.dart
@@ -0,0 +1,63 @@
+import 'package:querylib_test/querylib_test.dart';
+import 'package:dio/dio.dart';
+
+class Client implements Repository {
+  final Dio _dio;
+  final Storage _storage;
+  AuthModel? _lastAuth;
+
+  Client({required Dio dio, required Storage storage})
+    : _dio = dio,
+      _storage = storage;
+
+  Options get _options => Options(
+    headers: {if (lastAuth != null) 'Authorization': _lastAuth!.token},
+  );
+
+  @override
+  Future<ItemModel> getItemByID({required String id}) async {
+    final response = await _dio.get(
+      "${_storage.apiURL}/collections/items/records/$id",
+      options: _options,
+    );
+    return ItemModel.fromJSON(response.data);
+  }
+
+  @override
+  AuthModel? get lastAuth => _lastAuth;
+
+  @override
+  Future<AuthModel> login({
+    required String identity,
+    required String password,
+  }) async {
+    final response = await _dio.post(
+      '${_storage.apiURL}/collections/users/auth-with-password',
+      data: {'identity': identity, 'password': password},
+    );
+    _lastAuth = AuthModel.fromJSON(response.data);
+    return _lastAuth!;
+  }
+
+  @override
+  Future<void> logout() async {
+    _lastAuth = null;
+  }
+
+  @override
+  Future<UserModel> signup({
+    required String email,
+    required String password,
+    required String passwordConfirm,
+  }) async {
+    final response = await _dio.post(
+      '${_storage.apiURL}/collections/users/records',
+      data: {
+        'email': email,
+        'password': password,
+        'passwordConfirm': passwordConfirm,
+      },
+    );
+    return UserModel.fromJSON(response.data);
+  }
+}
