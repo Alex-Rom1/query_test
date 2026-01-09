0e1f7d29d1d538ba118acb21790e8a5024f5c0ff
Alex-rom1 <alex.rom495@gmail.com>
2026-01-10 00:30:44 +0300

    added query helper

--- lib/querylib_test2.dart
+++ lib/querylib_test2.dart
@@ -4,5 +4,8 @@ export 'src/data/models/auth_model.dart';
 export 'src/data/models/item_model.dart';
 
 export 'src/data/repository/repository.dart';
+export 'src/data/repository/client.dart';
 
 export 'src/data/storage/storage.dart';
+
+export 'src/domain/query_helper/query_helper.dart';
