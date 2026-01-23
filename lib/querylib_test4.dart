f777ea810731259a5c52ef8654ce962437d8ec68
Alex-rom1 <alex.rom495@gmail.com>
2026-01-23 20:57:32 +0300

    added repository

--- lib/querylib_test4.dart
+++ lib/querylib_test4.dart
@@ -2,3 +2,5 @@ export 'src/data/models/base_model.dart';
 export 'src/data/models/user_model.dart';
 export 'src/data/models/auth_model.dart';
 export 'src/data/models/note_model.dart';
+
+export 'src/data/repository/repository.dart';
---
+++ lib/src/data/repository/repository.dart
@@ -0,0 +1,5 @@
+import 'package:querylib_test4/querylib_test4.dart';
+
+abstract interface class Repository {
+  AuthModel? get lastAuth;
+}
