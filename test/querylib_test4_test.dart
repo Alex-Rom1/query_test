d89c8466ba28685a1d482c4e043e2cedcc7aba56
Alex-rom1 <alex.rom495@gmail.com>
2026-01-23 21:31:55 +0300

    added test group

--- test/querylib_test4_test.dart
+++ test/querylib_test4_test.dart
@@ -1,1 +1,20 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:querylib_test4/querylib_test4.dart';
 
+final _useCase = QueryUseCase(dio: Dio(.new(baseUrl: 'http://127.0.0.1:8090/api')));
+
+final email = '${DateTime.now().microsecondsSinceEpoch}@ya.ru';
+final password = r'Pa$$w0rd';
+
+void onResponse(dynamic obj, Type expectedType) {
+  stdout.writeln(obj.toString());
+  assert(obj.runtimeType == expectedType);
+}
+
+void onError(String e) => fail(e);
+
+void main(){
+  group('test query', (){});
+}
